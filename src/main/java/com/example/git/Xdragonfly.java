8b18b3e127c8689d6ba276689f390d9731869b8c
xdragonfly <zx_eclipse@126.com>
2018-07-18 10:21:26 +0800

    new class

---
+++ src/main/java/com/example/git/Xdragonfly.java
@@ -0,0 +1,31 @@
+package com.example.git;
+
+/**
+ * Created by zhaoxuan
+ * 2018/7/18 10:13
+ */
+public class Xdragonfly {
+    private String name;
+    private String age;
+
+    public Xdragonfly(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
